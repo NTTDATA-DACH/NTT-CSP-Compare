e441ad872bd5be55ce7c3ef63eda1cd371a486d0
Christoph Puppe / NTT <christoph.puppe@nttdata.com>
2026-01-24 18:42:06 +0000

    no job deploy in TF

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,11 +3,6 @@ output "bucket_name" {
   value       = google_storage_bucket.app_bucket.name
 }
 
-output "job_name" {
-  description = "The name of the Cloud Run Job"
-  value       = google_cloud_run_v2_job.pipeline_job.name
-}
-
 output "region" {
   description = "The GCP region"
   value       = var.region
